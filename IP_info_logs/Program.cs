6c339858409b3ef8eb9f60da0ecdfcf64dcf1a66
Денис Ангелов <angelovd357@gmail.com>
2023-08-08 13:16:27 +0300

    ip adderss exer

---
+++ IP_info_logs/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace IP_info_logs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IP_info();
+        }
+        static void IP_info()
+        {
+            SortedDictionary<string, string> IPLoggins = new SortedDictionary<string, string>();
+            SortedDictionary<string, int> IP_messageCount = new SortedDictionary<string, int>();
+            string loggin = Console.ReadLine();
+            while (loggin != "end")
+            {
+                int i = 3;
+                string ip = null;
+                string userName = null;
+                for (; i < loggin.Length; i++)
+                {
+                    if (loggin[i] == ' ')
+                    {
+                        i += 9;
+                        break;
+                    }
+                    else
+                    {
+                        ip += loggin[i];
+                    }
+                }
+                for(; i < loggin.Length; i++)
+                {
+                    if (loggin[i] == '=')
+                    {
+                        i++;
+                        break;
+                    }
+                }
+                for(; i < loggin.Length; i++)
+                {
+                    userName += loggin[i];
+                }
+                if(IPLoggins.ContainsKey(ip))
+                {
+                    IP_messageCount[ip]++;
+                }
+                else
+                {
+                    IPLoggins.Add(ip, userName);
+                    IP_messageCount.Add(ip, 1);
+                }
+                loggin = Console.ReadLine();
+            }
+            foreach(var item in IPLoggins)
+            {
+                TriplePair triplePair = new TriplePair();
+                triplePair.Name = item.Value;
+                triplePair.Value = item.Key;
+                triplePair.Count = IP_messageCount[item.Key];
+                Console.WriteLine($"{triplePair.Name}: {triplePair.Value} => {triplePair.Count}");
+            }
+        }
+    }
+    public struct TriplePair
+    {
+        public string Name;
+        public string Value;
+        public int Count;
+    }
+}
---
+++ IP_loggins_processing/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace IP_loggins_processing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IP_loggins();
+        }
+        static void IP_loggins()
+        {
+            SortedDictionary<string, int> timeSpend = new SortedDictionary<string, int>();
+            SortedDictionary<string, string> ipAdresses = new SortedDictionary<string, string>();
+            int inputCounts = int.Parse(Console.ReadLine());
+            for(int i = 0; i < inputCounts; i++)
+            {
+                string input = Console.ReadLine();
+                string ip = null;
+                string name = null;
+                int time = 0;
+                string num = null;
+                int j = 0;
+                for(; j < input.Length; j++)
+                {
+                    if (input[j] == ' ')
+                    {
+                        j++;
+                        break;
+                    }
+                    ip += input[j];
+                }
+                for(; j < input.Length; j++)
+                {
+                    if (input[j] == ' ')
+                    {
+                        j++;
+                        break;
+                    }
+                    name += input[j];
+                }
+                for(; j < input.Length; j++)
+                {
+                    num += input[j];
+                }
+                time = int.Parse(num);
+                if (timeSpend.ContainsKey(name))
+                {
+                    timeSpend[name]+=time;
+                    if(!ipAdresses.ContainsKey(ip))
+                    ipAdresses.Add(ip, name);
+                }
+                else
+                {
+                    timeSpend.Add(name, time);
+                    ipAdresses.Add(ip, name);
+                }
+            }
+            foreach(var item in timeSpend)
+            {
+                List<string> output = new List<string>();
+                foreach(var item2 in ipAdresses)
+                {
+                    if(item2.Value == item.Key)
+                    output.Add(item2.Key);
+                }
+                Console.WriteLine($"{item.Key}: {item.Value} [{String.Join(", ", output)}]");
+            }
+        }
+    }
+}
